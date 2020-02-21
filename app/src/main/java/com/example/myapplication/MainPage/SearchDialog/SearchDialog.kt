47f3a9a947fc9f5b16b05e421f037027650863e0
SEJEONG_12 <mozzi981231@gmail.com>
2020-02-21 20:50:08 +0900

    검색하면 관련 상품 뜨게 해놨음

--- app/src/main/java/com/example/myapplication/MainPage/SearchDialog/SearchDialog.kt
+++ app/src/main/java/com/example/myapplication/MainPage/SearchDialog/SearchDialog.kt
@@ -29,6 +29,10 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.search_dialog.*
 
 class SearchDialog(context: Context) : Dialog(context){
@@ -40,6 +44,8 @@ class SearchDialog(context: Context) : Dialog(context){
     lateinit var adapter:ItemListAdapter
     lateinit var list:ArrayList<ListItem>//검색
 
+    var database = FirebaseDatabase.getInstance() as FirebaseDatabase
+    var myRef = database.reference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -67,8 +73,10 @@ class SearchDialog(context: Context) : Dialog(context){
             //지도에도 마킹 해줘야댐
             var name = search_item.text.toString()//검색할 물품 이름
             //리스트를 만들어줌
+            Log.d("검색",name)
             //이름이 포함되는 물품
-            initLayout()
+            makeList(name)
+//            initLayout()
         }
 
 
@@ -83,5 +91,33 @@ class SearchDialog(context: Context) : Dialog(context){
         search_list.adapter = adapter
     }
 
+    fun makeList(name:String){
+        //유사 상품 리스트
+        list = arrayListOf()
+        Log.d("검색",myRef.toString())
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+                // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
 
+            override fun onDataChange(p0: DataSnapshot) {
+                // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                var value = p0.value as HashMap<String,HashMap<String,HashMap<String,*>>>
+                Log.d("검색",value.keys.toString())
+                //문자열이 포함되는지
+                for (category in value.keys){
+                    for(key in value[category]!!.keys){
+                        if(key.contains(name)){
+                            //문자열이 포함되면
+                            var price = value[category]!![key]!!["price"].toString().replace(",","")
+                            list.add(ListItem(value[category]!![key]!!["img"].toString(),key,0,price.toInt(),""))
+                        }
+                    }
+                }
+                Log.d("검색","for끝")
+                initLayout()
+            }
+
+        })
+    }
 }
